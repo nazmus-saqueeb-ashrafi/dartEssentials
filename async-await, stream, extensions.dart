8c0adb65cfddae4279633dcdbed0fb288d6eef10
Nazmus Saqueeb Ashrafi <53981051+nazmus-saqueeb-ashrafi@users.noreply.github.com>
2022-12-04 18:18:10 +0600

    Create async-await, stream, extensions.dart

---
+++ async-await, stream, extensions.dart
@@ -0,0 +1,37 @@
+// extensions ----------
+class Person {
+  final String firstName;
+  final String lastName;
+  Person(this.firstName, this.lastName);
+}
+
+extension FullName on Person {
+  String get fullName => '$firstName $lastName';
+}
+
+void test() {
+  print("We are TESTING EXTENSION");
+
+  final person1 = Person("Nazmus", "Ashrafi");
+  print(person1.fullName);
+}
+
+
+// async functions -----------
+int multiplyTwo(int a, int b) => a * b; // synchoronous operation
+
+Future<int> multiplyTwoHeavy(int a, int b) {
+  return Future.delayed(const Duration(seconds: 3), (() => a * b));
+} // async operation
+
+/* marking async means this function can execute commands that do not 
+return immediately */
+void test() async {
+  print("We are TESTING ASYNCHRONOUS FUNCTION");
+  final result = await multiplyTwoHeavy(10, 15);
+  print(result);
+}
+
+
+// stream -------------
+// an asynchronous pipe of data
